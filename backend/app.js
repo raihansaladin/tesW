422b8a2700117614a22ea536e1771ad26a2f4514
Raihan <raihansaladin001@gmail.com>
2025-08-29 22:05:28 +0800

    endpoin post

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
    res.send('Hello World!!');
 })
 
+app.post('/data', (req, res) => {
+  res.send('Data berhasil diterima!');
+});
+
+app.get('/about', (req, res) => {
+  res.json({ message: "Ini endpoint About" });
+});
+
 app.listen(port, () => {
    console.log(`Example App Listen to ${port}`);
 })
+
+//        ../node_modules/long/ .
+//        ../node_modules/lru-cache/.
+//        ../node_modules/lru.min/.
+//        ../node_modules/mysql2/.
+//        ../node_modules/named-placeholders/.
+//        ../node_modules/seq-queue/.
+//        ../node_modules/sqlstring/.
